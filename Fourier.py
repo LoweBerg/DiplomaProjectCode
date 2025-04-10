b2e5a66833a462bd2d4bbbc2afc8b53edf6a9987
Lowe Berg <loweberg04@gmail.com>
2025-04-10 15:27:37 +0200

    Uploaded finalized project files

--- Fourier.py
+++ Fourier.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+from scipy import stats
 import numpy as np
+import xlsxwriter as xl
 
 sample_frequency = 50
-samples = 10
+samples = 50
 
 df_rank = [0] * 10
 dt_rank = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 sr_n = len(df_rank)
 
-plot_all = True
+plot_all = False
 
 x = []  # delta time (s)
 y = []  # delta frequency (Hz)
+xf = []
+yf = []
+
 
 for _ in range(samples):
 
-    sampFreq, sound = wavfile.read(f'Tapered/sample{_}.wav')
+    sampFreq, sound = wavfile.read(f'newSine/sample{_+1}.wav')
 
     sound = sound / 2.0**15  # normalize amplitudes
 
@@ -35,7 +40,7 @@ for _ in range(samples):
     start = 0
 
     for i, f in enumerate(xf):
-        if f == sample_frequency:
+        if round(f) == sample_frequency:
             start = i
             break
 
@@ -49,48 +54,52 @@ for _ in range(samples):
     while yf[x_upper + 1] < yf[x_upper]:
         x_upper += 1
 
-    dx = xf[x_upper] - xf[x_lower]
+    if x_lower == 0:
+        dx = (xf[x_upper] - xf[start]) * 2
+    else:
+        dx = xf[x_upper] - xf[x_lower]
 
     y.append(dx)
 
-    print(f"---sample {_}---")
+    print(f"---sample {_+1}---")
     print(f"sample frequency: {sampFreq}Hz")
     print(f"duration: {duration}s")
     print(f"dx: {dx}Hz")
 
-    if plot_all:
-        plt.figure(f"sample {_+1}")
+    if plot_all or x_lower < 0:
+        plt.figure(f"sample {_+1}", facecolor="#EEEEEE", figsize=(8, 3))
+        plt.title(f"Sample {_+1} Fourier Transform",
+                  fontname="Times New Roman",
+                  weight="bold",)
 
-        sound_samples = np.arange(len(sound))
+        # sound_samples = np.arange(len(sound))
 
-        plt.subplot(2, 1, 1)
-        plt.plot(sound_samples/sampFreq, sound)
-        plt.xlabel("t(s)")
+        # plt.subplot(2, 1, 1)
+        # plt.plot(sound_samples/sampFreq, sound)
+        # plt.xlabel("t(s)")
 
-        plt.subplot(2, 1, 2)
+        # plt.subplot(2, 1, 2)
         plt.plot(xf, yf, xf[x_lower], yf[x_lower], 'or', xf[x_upper], yf[x_upper], 'ob')
         plt.xlim(0, 300)
-        plt.xlabel("Frequency (Hz)")
-        plt.ylabel("Amplitude")
+        plt.xlabel("Frequency (Hz)", labelpad=10)
+        plt.ylabel("Relative Amplitude")
+        plt.tight_layout()
 
-d_i = 0
+# noinspection PyTypeChecker
+rank_coef, p_value = stats.spearmanr(x[1:], y[1:])  # first sample will be ignored as an outlier
 
-highest = 0
-for i in range(10, 0, -1):
-    for j in range(0, 10):
-        if y[j] > y[highest]:
-            highest = i
+print("Rs:", rank_coef)
 
-
-for i in range(sr_n):
-    d_i += (df_rank[i] - dt_rank[i])**2
-
-spearman_rank = 1 - (6 * d_i) / (sr_n * (sr_n**2 - 1))
-
-print("Rs:", spearman_rank)
+# writing to excel sheet
+workbook = xl.Workbook("C:/Users/loweb/DiplomaProject/test.xlsx")
+worksheet = workbook.add_worksheet()
+worksheet.write_row(1, 1, x)
+worksheet.write_row(2, 1, y)
+workbook.close()
 
 
-x2 = np.linspace(0.000001, 1.000001)
+x2 = np.linspace(0, 1.1, 100)
+x2 = x2[1:]
 
 plt.figure("Results", facecolor="#EEEEEE")
 plt.title(r"Uncertainty in Frequency for Sounds with Different Durations",
@@ -100,8 +109,8 @@ plt.title(r"Uncertainty in Frequency for Sounds with Different Durations",
           pad=20)
 plt.xlabel(r"Sound Duration $\Delta$t(s)", weight="bold", bbox=dict(facecolor="#FBE4D5"))
 plt.ylabel(r"Uncertainty in Frequency $\Delta$f(Hz)", weight="bold", bbox=dict(facecolor="#D9E2F3"))
-plt.xlim(0, 1)
-plt.ylim(0, 50)
+plt.xlim(0, 1.1)
+plt.ylim(0, 110)
 plt.xticks(np.arange(0, 1.1, 0.1))
 plt.plot(x, y, 'or')
 plt.plot(x2, 4.0 / x2, label=r'$\frac{4}{\Delta t}$')
